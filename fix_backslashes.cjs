45cb4586b26d6d956c600aa79bf5dc97342f65bb
wmdstone <jankimdst@gmail.com>
2026-04-29 23:15:19 +0700

    cleaning-ai

--- fix_backslashes.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/admin/AdminStudentsTab.tsx', 'utf8');
-content = content.replace(/\\`/g, '`').replace(/\\\$/g, '$');
-fs.writeFileSync('src/components/admin/AdminStudentsTab.tsx', content);
