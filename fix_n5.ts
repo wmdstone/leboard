7af3d1838a2df3d0832302e63459f6906a2fc61a
wmdstone <jankimdst@gmail.com>
2026-04-30 22:14:49 +0700

    clean

--- fix_n5.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-let content = fs.readFileSync('src/components/AdminBackendTab.tsx', 'utf-8');
-let lines = content.split('\\n');
-
-// Line 1231 contains `}` which is index 1230.
-lines = lines.slice(0, 1231);
-
-fs.writeFileSync('src/components/AdminBackendTab.tsx', lines.join('\\n') + '\\n');
